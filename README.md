b08a4e82eeab83827d28ca9a4400d6d1117bac3c
Tioreza Febrian <tiorezafk@outlook.com>
2023-12-17 21:30:39 +0700

    add gitignore

--- README.md
+++
@@ -1,7 +0,0 @@
-# How to run ?
-
-## 1. copy .env.example and rename to .env
-
-## 2. npm install
-
-## 3. npm run dev (default port 8000)
--- jest-puppeteer.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    launch: {
-        headless: false,
-        product: 'chrome',
-        args: ['--start-maximized'],
-        defaultViewport :{width: 1700, height: 800 }
-    },
-    browserContext: 'default',
-}
